c86d1ccb8ad7605c90a3eebf608d1a8d6cccae83
HenryCastillo <hdaniel031499@gmail.com>
2022-06-17 12:03:41 -0500

    Conectamos con Firebase/users.json y traemos la data

--- src/app/servicios/users.service.ts
+++ src/app/servicios/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+// importar el HttpClient
+import { HttpClient } from '@angular/common/http';
+// importar environmnet
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  // Traer la data de la coleccion de usuarios en Firebase
+  getData() {
+    return this.http.get(`${environment.urlFirebase}users.json`);
+  }
 }
+
