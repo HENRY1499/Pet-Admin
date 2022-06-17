bea9cc1f14c25e07ffe56af7a9eda8929ea3bbf5
HenryCastillo <hdaniel031499@gmail.com>
2022-06-17 10:39:31 -0500

    Creación de la interface de users llamado Iuser

---
+++ src/app/interface/iusers.ts
@@ -0,0 +1,12 @@
+export interface Iusers {
+    picture: ImageBitmap,
+    firts_name: string,
+    last_name: string,
+    username: string,
+    email: string,
+    country: string,
+    state: string,
+    city: string,
+    phone: string,
+    address: string
+}
