2c774e81f8db70d801b50698b800a231250954dc
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 18:02:03 -0500

    Configuramos el modulo de Adopciones

--- src/app/pages/main-pages/adopciones/adopciones.module.ts
+++ src/app/pages/main-pages/adopciones/adopciones.module.ts
@@ -5,12 +5,15 @@ import { CommonModule } from '@angular/common';
 import { AdopcionesComponent } from './adopciones.component';
 // importamos el AdopcionesRouting
 import { AdopcionesRoutingModule } from './adoption-routing.module';
-
+import { MatTableModule } from '@angular/material/table';
+import { PipesModule } from 'src/app/Pipes/pipes.module';
 @NgModule({
   declarations: [AdopcionesComponent],
   imports: [
     CommonModule,
-    AdopcionesRoutingModule
+    AdopcionesRoutingModule,
+    PipesModule,
+    MatTableModule
   ]
 })
 export class AdopcionesModule { }
