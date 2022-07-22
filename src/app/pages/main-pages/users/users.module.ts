2e90de843a46e0438581e5c183e388d2355fab03
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 12:27:31 -0500

    Importamos MatTableModule,UserComponent,Pipes

--- src/app/pages/main-pages/users/users.module.ts
+++ src/app/pages/main-pages/users/users.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// importar recuerso de material
+import { MatTableModule } from '@angular/material/table';
 // importamos componente users
 import { UsersComponent } from './users.component';
 // importamos erouting
 import { UsersRoutingModule } from './users-routing.module';
+// importamos pipes
+import { PipesModule } from 'src/app/Pipes/pipes.module';
 @NgModule({
   declarations: [UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    MatTableModule,
+    PipesModule
   ]
 })
 export class UsersModule { }
