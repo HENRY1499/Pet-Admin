4c0a0c0b5b0ec4469bf5569bf713b9c4546387ca
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 16:04:30 -0500

    Creamos la interfaz de Refugios(shelters)

---
+++ src/app/interface/irefugios.ts
@@ -0,0 +1,13 @@
+export interface Irefugios {
+    about: string,
+    abstract: string,
+    addres: string,
+    cover: string,
+    email: string,
+    logo: ImageBitmap,
+    pets: number
+    phone: string,
+    shelter: string,
+    social: string,
+    username: string
+}
