d33bc029c9adfe15338860d64af8a64a20cd2457
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 11:37:44 -0500

    Creamos los Pipes y pipe.module.ts

---
+++ src/app/Pipes/pipe.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'pipe'
+})
+export class PipePipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    if (value) {
+      let arr = JSON.parse(value);
+      let str = "";
+      for (const i in arr) {
+        str += arr[i] + ", ";
+      }
+      str = str.slice(0, -2);
+      return str;
+    }
+  }
+
+}
