8b2c497e1b3b637143c57630964d95da0407adb8
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 12:19:15 -0500

    Importamos MatTableModule, CategoriaComponent, CategoriasRoutingModule,PipesModule

--- src/app/pages/main-pages/categorias/categorias.module.ts
+++ src/app/pages/main-pages/categorias/categorias.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// importacion recursos de material
+import { MatTableModule } from '@angular/material/table';
 // importamos el componente
 import { CategoriasComponent } from './categorias.component';
 // importamos el CategoriaRoutingModule
 import { CategoriasRoutingModule } from './categorias-routing.module';
+// importamos pipes
+import { PipesModule } from 'src/app/Pipes/pipes.module';
 @NgModule({
   declarations: [CategoriasComponent],
   imports: [
     CommonModule,
-    CategoriasRoutingModule
+    CategoriasRoutingModule,
+    MatTableModule,
+    PipesModule
   ]
 })
 export class CategoriasModule { }
