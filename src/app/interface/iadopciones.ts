94c6f631ae754b0bac495db43f5370bc4f5360b9
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 17:55:05 -0500

    Creamos la interfaz de Adopciones

---
+++ src/app/interface/iadopciones.ts
@@ -0,0 +1,13 @@
+export interface Iadopciones {
+    client: string
+    commision: number,
+    id_order: number,
+    id_payment: string,
+    payment_method: string,
+    pets_product: string,
+    quantity: number,
+    total: number,
+    unit_price: number,
+
+
+}
