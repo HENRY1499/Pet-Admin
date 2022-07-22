42850b91a0ee2f1d7a75592c43ca49dbabcf3c5d
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 11:40:12 -0500

    Creamos la interfaz de subcategoria

---
+++ src/app/interface/isubcategoria.ts
@@ -0,0 +1,9 @@
+export interface Isubcategoria {
+    category: string,
+    image: ImageBitmap,
+    name: string,
+    products_inventory: number,
+    title_list: string,
+    url: string,
+    view: number
+}
