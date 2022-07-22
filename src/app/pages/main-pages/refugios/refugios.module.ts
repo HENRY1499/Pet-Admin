46cbb515c4f2658d30c5510f6366ef9aa130495c
HenryCastillo <hdaniel031499@gmail.com>
2022-07-22 16:32:52 -0500

    Importe MatTableModule y Pipes

--- src/app/pages/main-pages/refugios/refugios.module.ts
+++ src/app/pages/main-pages/refugios/refugios.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// importamos rel routing refugios
+import { RefugiosRoutingModule } from './refugios-routing.module';
 
 // importamos el componente refugio
 import { RefugiosComponent } from './refugios.component';
-// importamos rel routing refugios
-import { RefugiosRoutingModule } from './refugios-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { PipesModule } from 'src/app/Pipes/pipes.module';
+
+
 @NgModule({
   declarations: [RefugiosComponent],
   imports: [
     CommonModule,
-    RefugiosRoutingModule
+    RefugiosRoutingModule,
+    MatTableModule,
+    PipesModule
   ]
 })
 export class RefugiosModule { }
